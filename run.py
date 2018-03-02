0e161c2e7e8ef97e4c4eb94b3f2628e356bffec9
jack andrews <jandrews@kx.com>
2018-03-02 18:11:24 +1100

    got the 3 simple tests running

--- run.py
+++ run.py
@@ -3,13 +3,16 @@ from re import *
 from time import *
 import os
 
+g=0
+
 #reach - increase n until time exceeds 1 second
 def reach(exe,test):
   n=1;t=-1;rc=-1;seed=-1;r=0
   while n<32:
     seed=int(1e9*clock_gettime(CLOCK_MONOTONIC_RAW)%pow(2,31))
     env=dict(os.environ,TIMEFORMAT='%R %U %S')
-    p=Popen([' '.join(['time',exe,str(seed),str(test),str(n)])], stdout=PIPE, stderr=PIPE, shell=True, executable="/bin/bash",env=env)
+    cmd=' '.join(['time',exe,str(test),str(n),str(seed)])
+    p=Popen([cmd], stdout=PIPE, stderr=PIPE, shell=True, executable="/bin/bash",env=env)
     try:
       (o,e)=p.communicate(timeout=3)
     except TimeoutExpired:
@@ -17,6 +20,7 @@ def reach(exe,test):
       (o,e)=p.communicate()
       return 0
     rc=p.returncode
+    if(g):print(cmd,'returned',str(rc),"out",o)
     if rc!=0:break
     r=int(fullmatch(b'[0-9]+\n',o).group(0))
     b=b'([0-9.:]+)[^ ]* '
@@ -27,3 +31,6 @@ def reach(exe,test):
 
 
 print(reach('./ref',0))
+print(reach('./ref',1))
+print(reach('./ref',2))
+#[lambda x:print(reach('./ref',x)) for x in range(3)]
