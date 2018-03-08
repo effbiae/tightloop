95f36fcb8e83c0c19ba5f1d3006ff0c1bc178efa
jack andrews <jandrews@kx.com>
2018-03-08 15:33:44 +1100

    tidy

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from terminaltables import *
 from functools import *
 from getopt import getopt
 from sys import *
-from statistics import median
+from statistics import *
 
 from sqlite3 import dbapi2 as sql
 class med:
@@ -21,10 +21,8 @@ class gmean:
 conn = sql.connect(':memory:')
 import math
 conn.create_function("log10",1,math.log10)
-conn.create_function("sqrt",1,math.sqrt)
 conn.create_aggregate("median",1,med)
 conn.create_aggregate("gmean",1,gmean)
-conn.create_function("pow",2,math.pow)
 conn.create_function("pow10",1,lambda x:math.pow(10,x))
 c = conn.cursor()
 
@@ -57,39 +55,38 @@ def tim(exe,test,n,seed):
     print(e)
     raise Exception("match")
   return [exe,test,n if n<32 and not rc else -1,seed,r,t]
+def ti(pre,test,n,seed):
+  x="./ref" if pre=="ref" else './contrib/'+pre+'/'+pre
+  return [pre]+tim(x,test,n,seed)[1:]
 
 #reach - increase n until time exceeds 1 second
-def reach(exe,test):
+def reach(pre,test):
   n=1;t=-1;rc=-1;seed=-1;r=0
   while n<32:
-    seed=int(1e9*clock_gettime(CLOCK_MONOTONIC_RAW)%pow(2,31))
-    r=tim(exe,test,n,seed)
+    seed=int(1e9*clock_gettime(CLOCK_MONOTONIC)%pow(2,16))
+    r=ti(pre,test,n,seed)
     if r[-1]>oo['r']:break
     n+=1
   return r
 
-def gmean(a):
-  return reduce(lambda x,y:x*y,a)**(1.0/len(a))
-def tims(exe,test,n,seeds):
-  x=[tim(exe,test,n,seed)for seed in seeds]
-  return x[0][:-1]+[round(gmean([a[-1]for a in x]),3)]
+#def gmean(a):
+#  return reduce(lambda x,y:x*y,a)**(1.0/len(a))
 def tab(c,sql="select * from t"):
   c.execute(sql)
   cols=[x[0] for x in c.description]
   t=AsciiTable([cols]+c.fetchall())
   for x in range(len(cols)):t.justify_columns[x]='right'
   print(t.table)
+def raze(x):return reduce(lambda x,y:x+y,x)
 
-r=[reach('./ref',x) for x in range(3)]
-T=([tims(*r[:3]+[[r[3]]*3]) for r in r])
+r=[reach('ref',x) for x in range(3)]
+T=raze([[ti(*x[:4]) for a in range(3)]for x in r])
 c.execute('create table t(exe,test,n,seed,result,time)')
-c.executemany("insert into t values(?,?,?,?,?,?)",T)
+def tins(x):return c.executemany("insert into t values(?,?,?,?,?,?)",x)
+tins(T)
 tab(c)
 
 t=['jfa','cpp']
-U=([[[x]+tims(*['./contrib/'+x+'/'+x]+r[1:3]+[[r[3]]*3])[1:]for r in r]for x in t*3])
-[c.executemany('insert into t values(?,?,?,?,?,?)',x) for x in U]
-tab(c,'select * from t order by test,n,time asc')
-
-c.execute('select exe,pow10(sum(time)) from (select exe,test,log10(time)as time from t)group by exe order by time')
-tab(c,'select exe,gmean(time) from (select exe,test,median(time) as time from t group by exe,test) group by exe order by time')
+U=([[[x]+ti(*[x]+r[1:4])[1:]for r in r]for x in t*3])
+tins(raze(U))
+tab(c,'select exe,gmean(time)as gm from (select exe,test,median(time) as time from t group by exe,test) group by exe order by gm')
